570d428ebabbb803d29e534fc000cd9cc784720b
Jramos <jramos@saludelectronica.com>
2022-12-15 20:24:53 -0500

    Se agrega el envio de correos y el guardado de usuarios.

--- src/users/service/users.service.ts
+++ src/users/service/users.service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { EmailService } from 'src/core/email.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private emailService: EmailService,
+  ) {}
   async create(dataUser: CreateUserDto) {
     try {
       const newUser = new this.userModel(dataUser);
       await newUser.save();
+      await this.sendEmail(dataUser);
     } catch (error) {
-      console.log(error);
+      throw new InternalServerErrorException('Problema no controlado');
     }
-    return 'This action adds a new user';
+    return {
+      message: 'Usuario creado exitosamente.',
+      statusCode: 201,
+    };
+  }
+
+  async sendEmail(dataUser: CreateUserDto) {
+    try {
+      await this.emailService.sendMailAdvisory(dataUser);
+    } catch (error) {
+      throw new InternalServerErrorException('Problema no controlado');
+    }
+    return;
   }
 }
